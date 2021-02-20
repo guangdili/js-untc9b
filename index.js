68dae22e1d2da6708a422e73322bd940794c435a
guangdili <guangdili@aim.com>
2021-02-20 17:09:56 -0500

    1 file modified

--- index.js
+++ index.js
@@ -42,3 +42,79 @@ appDiv.innerHTML = `<table>
             </tr>
         </tbody>
     </table>`;
+
+var acctData = [
+    {
+        "acctNum": "AAA - 1234",
+        "user": "Alice"
+    },
+    {
+        "acctNum": "AAA - 5231",
+        "user": "Bob"
+    },
+    {
+        "acctNum": "AAA - 9921",
+        "user": "Alice"
+    },
+    {
+        "acctNum": "AAA - 8191",
+        "user": "Alice"
+    }
+];
+
+var balance = {
+    "AAA - 1234": 4593.22,
+    "AAA - 9921": 0,
+    "AAA - 5231": 232142.5,
+    "AAA - 8191": 4344
+};
+
+function sortFilterData(user, sortBy, sortDirection = "asc") {
+    var array;
+
+    if (sortBy === "undefined" || sortBy === "")
+        return;
+    else if (sortBy === "acctNum") {
+        array = acctData;
+        //sort asc at the first
+        array.sort((a, b) => (a.acctNum > b.acctNum) ? 1 : (a.acctNum === b.acctNum) ? ((a.user > b.user) ? 1 : -1) : -1);
+        if (sortDirection === "desc") {
+            // Then reverse it as desc:
+            array.reverse();
+        }
+        acctData = array;
+    }
+    else if (sortBy === "balance") {
+        array = balance;
+        //sort asc at the first
+        array = Object.entries(array).sort((a, b) => (a[1] - b[1]));       
+        if (sortDirection === "desc") {
+            // Then reverse it as desc:
+            array.reverse();
+        }
+        balance = array;
+    }
+    console.log(array);
+    //filter user
+    if (user === "undefined" || user === "")
+        return;
+    else
+    {
+        var array = acctData;
+        console.log(filterObj(array, user));
+    }
+}
+
+function filterObj(array, user) {
+    return array.filter(x => x.user === user);
+}
+
+var user = null;
+/*Copy all into you brower's console. Remove test a case common to test.*/
+//sortFilterData("Bob");
+//sortFilterData("Charlie");
+//sortFilterData(user, "acctNum");
+//sortFilterData(user, "acctNum", "desc");
+//sortFilterData(user, "balance");
+//sortFilterData(user, "balance","desc");
+//sortFilterData("Alice", "balance");
